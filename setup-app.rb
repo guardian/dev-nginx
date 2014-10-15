657c8e44f45ccfc0aebe10eb18ad5d62a40bbe1c
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-10-15 08:52:33 +0100

    Disable SSLv3 which is unsafe, use TLS only

--- setup-app.rb
+++ setup-app.rb
@@ -53,7 +53,7 @@ EOS
 
   ssl_session_timeout 5m;
 
-  ssl_protocols SSLv2 SSLv3 TLSv1;
+  ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
   ssl_ciphers HIGH:!aNULL:!MD5;
   ssl_prefer_server_ciphers on;
 EOS
