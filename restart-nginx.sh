06ed58d7dea134d1012d4defb98b17caac5e616b
Akash Askoolum <akash1810@gmail.com>
2019-03-11 17:46:57 +0000

    add note about sudo access to restart nginx

--- restart-nginx.sh
+++ restart-nginx.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-nginx -s stop
-nginx
+# colours
+YELLOW='\033[1;33m'
+NC='\033[0m' # no colour - reset console colour
+
+echo -e "${YELLOW}Restarting nginx. This requires sudo access.${NC}"
+
+sudo nginx -s stop
+sudo nginx
